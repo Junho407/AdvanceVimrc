d9d976bf53ba5b6b97f3bc04177af5f7c340eb79
Junho407 <junho407@gmail.com>
2019-09-24 13:29:02 +0800

    Removed deprecated lookup file script

--- genfiletags.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Generate tag file for lookupfile plugin
-echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > filenametags
-find . -regex '.*\.\(h\|c\|html\|asp\|js\|css\|def\|mk\|cfg\)' ! -path "*svn*" -type f -printf "%f\t%p\t1\n" | sort -f>> filenametags
-#find $PWD -regex '.*\.\(h\|c\|html\|asp\|js\|css\|def\|mk\|cfg\)' ! -path "*svn*" -type f -printf "%f\t%p\t1\n" | sort -f>> filenametags
-#mv filenametags $CSCOPE_DIR
